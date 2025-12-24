32a447c522e13c2a1cf14173d81d6b5c8267ffe4
Guilherme Sales <guilhermedesaales@gmail.com>
2025-12-24 14:37:21 -0300

    :sparkles:(linhas):melhoria na UI/UX da busca de linhas

--- NoPonto/app/linhas.tsx
+++ NoPonto/app/linhas.tsx
@@ -14,8 +14,9 @@ import {
   Train,
   TrainFrontTunnel,
 } from "lucide-react-native";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import {
+  DimensionValue,
   FlatList,
   Keyboard,
   Pressable,
@@ -27,7 +28,7 @@ import MapView from "react-native-maps";
 import Animated, { FadeInUp, LinearTransition } from "react-native-reanimated";
 
 const linhas = () => {
-  const [modal, setModal] = useState<String | null>("Onibus");
+  const [modal, setModal] = useState<string | null>("Onibus");
   const [placeholder, setPlaceholder] = useState(
     "Selecione um tipo de Transporte"
   );
@@ -101,28 +102,47 @@ const linhas = () => {
 
   const scrollRef = React.createRef<FlatList>();
 
-const scrollDown = () => {
-  setTimeout(() => {
-    if (scrollRef.current) {
-      scrollRef.current.scrollToOffset({
-        offset: 550,
-        animated: true,
-      });
+  const scrollDown = () => {
+    setTimeout(() => {
+      if (scrollRef.current) {
+        scrollRef.current.scrollToOffset({
+          offset: 550,
+          animated: true,
+        });
+      }
+    }, 400);
+  };
+
+  const [telaLinhas, setTelaLinhas] = useState<DimensionValue>("50%");
+
+  useEffect(() => {
+    const tecladoAberto = Keyboard.addListener("keyboardDidShow", () => {
+      setTelaLinhas("80%");
+    });
+    const tecladoFechado = Keyboard.addListener("keyboardDidHide", () => {
+      if (sentidoSelecionado) {
+        setTelaLinhas("75%");
+      } else if (linhaSelecionada) {
+        setTelaLinhas("55%");
+      } else setTelaLinhas("50%");
+    });
+
+    return () => {
+      tecladoAberto.remove();
+      tecladoFechado.remove();
+    };
+  }, [linhaSelecionada, sentidoSelecionado]);
+
+  useEffect(() => {
+    if (sentidoSelecionado && linhaSelecionada) {
+      setTelaLinhas("75%");
     }
-  }, 400);
-};
+  }, [sentidoSelecionado]);
 
   return (
-    <View className=" flex-1 bg-white">
+    <View className=" flex flex-col h-screen rounded-t-3xl overflow-hidden">
       <MapView
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 600,
-          zIndex: 0,
-        }}
+        style={{ flex: 1 }}
         mapType="standard" // tipo de mapa
         showsUserLocation={true}
         followsUserLocation={true}
@@ -135,111 +155,118 @@ const scrollDown = () => {
           },
         ]}
         initialRegion={{
-          latitude: -22.512384145462976,
-          longitude: -43.22661972224441,
+          latitude: -22.882384145462976,
+          longitude: -43.16511972224441,
           latitudeDelta: 0.2,
           longitudeDelta: 0.2,
         }}
       />
 
       {/*container de linhas*/}
+      <Animated.View
+        style={{ height: telaLinhas as DimensionValue }}
+        className="flex-col bg-customGray rounded-t-3xl shadow-lg overflow-hidden"
+      >
+        {/* container do select modal */}
+        <View className="pt-6 pb-2 mb-3 bg-customGray rounded-t-3xl overflow-hidden">
+          <Text className="mt-5 text-2xl font-semibold self-center">
+            Linhas e Hórarios
+          </Text>
 
-      <FlatList
-        ref={scrollRef}
-        scrollEnabled={!buscaAtiva}
-        className="absolute rounded-3xl w-full h-[75%] bottom-0 mb-8 bg-customGray z-10"
-        showsVerticalScrollIndicator={false}
-        keyboardShouldPersistTaps="handled"
-        data={[{ key: "content" }]}
-        //renderItem={null}
-        keyExtractor={(item) => item.key}
-        contentContainerStyle={{ paddingBottom: 80 }}
-        renderItem={() => (
-          <>
-            <Text className="mt-7 text-2xl font-semibold self-center">
-              Linhas e Hórarios
-            </Text>
+          <SelectTransporte modal={modal} setModal={setModal} />
+        </View>
 
-            <SelectTransporte modal={modal} setModal={setModal} />
+        <FlatList
+          ref={scrollRef}
+          scrollEnabled={!buscaAtiva}
+          className="flex-1"
+          showsVerticalScrollIndicator={false}
+          keyboardShouldPersistTaps="handled"
+          data={[{ key: "content" }]}
+          //renderItem={null}
+          keyExtractor={(item) => item.key}
+          contentContainerStyle={{ paddingBottom: 80 }}
+          renderItem={() => (
+            <>
+              {/*input buscar linha*/}
+              <View className="bg-customGray mt-2 pt-5 ">
+                <InputBusca
+                  placeholder={placeholder}
+                  icon={icon}
+                  className="!w-[90%] self-center mb-5"
+                  value={busca}
+                  onChangeText={buscarLinhas}
+                />
 
-            {/*input buscar linha*/}
-            <View className="bg-customGray mt-2 pt-5 ">
-              <InputBusca
-                placeholder={placeholder}
-                icon={icon}
-                className="!w-[90%] self-center mb-5"
-                value={busca}
-                onChangeText={buscarLinhas}
-              />
+                {buscaAtiva && (
+                  <Animated.View
+                    entering={FadeInUp.duration(400).springify()}
+                    layout={LinearTransition}
+                    className="h-[150] bg-white rounded-xl mx-10 w-[90%] self-center shadow-sm border border-gray-200 overflow-hidden mb-4"
+                  >
+                    <ScrollView
+                      nestedScrollEnabled={true}
+                      keyboardShouldPersistTaps="handled"
+                      showsVerticalScrollIndicator={true}
+                    >
+                      {data.map((item) => (
+                        <Pressable
+                          key={item.id}
+                          onPress={() => listaSelecionada(item)}
+                          className="p-4 border-b border-gray-100 active:bg-gray-200"
+                        >
+                          <Text className="text-lg font-semibold">
+                            {item.nome}
+                          </Text>
+                          <Text className="text-gray-500 text-sm">
+                            {item.sentido}
+                          </Text>
+                        </Pressable>
+                      ))}
+                    </ScrollView>
+                  </Animated.View>
+                )}
 
-              {buscaAtiva && (
+                <Select
+                  placeholder="Selecione o Sentido"
+                  className="!w-[90%] self-center"
+                  options={sentido()}
+                  value={sentidoSelecionado}
+                  onChange={setSentidoSelecionado}
+                />
+              </View>
+
+              {/*container de resultado*/}
+              {linhaSelecionada && sentidoSelecionado && (
                 <Animated.View
                   entering={FadeInUp.duration(400).springify()}
-                  layout={LinearTransition}
-                  className="h-[150] bg-white rounded-xl mx-10 w-[90%] self-center shadow-sm border border-gray-200 overflow-hidden mb-4"
+                  className="w-full mt-8 h-full"
                 >
-                  <ScrollView
-                    nestedScrollEnabled={true}
-                    keyboardShouldPersistTaps="handled"
-                    showsVerticalScrollIndicator={true}
-                  >
-                    {data.map((item) => (
-                      <Pressable
-                        key={item.id}
-                        onPress={() => listaSelecionada(item)}
-                        className="p-4 border-b border-gray-100 active:bg-gray-200"
-                      >
-                        <Text className="text-lg font-semibold">
-                          {item.nome}
-                        </Text>
-                        <Text className="text-gray-500 text-sm">
-                          {item.sentido}
-                        </Text>
-                      </Pressable>
-                    ))}
-                  </ScrollView>
+                  <Text className="left-6 font-semibold mb-4 text-lg">
+                    Linha {busca} - {sentidoSelecionado}
+                  </Text>
+
+                  <Chegada intervalo={linhaSelecionada.intervalo} />
+
+                  <Tarifas
+                    valor={linhaSelecionada.tarifa}
+                    modal={linhaSelecionada.modal}
+                  />
+
+                  <PontosInteresses // deu erro de tipagem, gemini arrumou, tenho q pesquisar mais sobre isso
+                    pontos={
+                      pontosPorLinha[
+                        linhaSelecionada.nome as keyof typeof pontosPorLinha
+                      ]
+                    }
+                    scrollDown={scrollDown}
+                  />
                 </Animated.View>
               )}
-
-              <Select
-                placeholder="Selecione o Sentido"
-                className="!w-[90%] self-center"
-                options={sentido()}
-                value={sentidoSelecionado}
-                onChange={setSentidoSelecionado}
-              />
-            </View>
-
-            {/*container de resultado*/}
-            {linhaSelecionada && sentidoSelecionado && (
-              <Animated.View
-                entering={FadeInUp.duration(400).springify()}
-                className="w-full mt-8 h-full"
-              >
-                <Text className="left-6 font-semibold mb-4 text-lg">
-                  Linha {busca} - {sentidoSelecionado}
-                </Text>
-
-                <Chegada intervalo={linhaSelecionada.intervalo} />
-
-                <Tarifas
-                  valor={linhaSelecionada.tarifa}
-                  modal={linhaSelecionada.modal}
-                />
-
-                <PontosInteresses // deu erro de tipagem, gemini arrumou, tenho q pesquisar mais sobre isso
-                  pontos={
-                    pontosPorLinha[
-                      linhaSelecionada.nome as keyof typeof pontosPorLinha
-                    ]
-                  }
-                  scrollDown={scrollDown}
-                />
-              </Animated.View>
-            )}
-          </>
-        )}
-      />
+            </>
+          )}
+        />
+      </Animated.View>
     </View>
   );
 };
